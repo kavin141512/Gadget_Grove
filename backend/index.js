195ac8d98e585d4cd58ef3a0942cb0e40b547ec1
laxmanp090404 <laxman.p2022cse@sece.ac.in>
2024-03-03 18:07:31 +0530

    Signup And Login Apis Created

--- backend/index.js
+++ backend/index.js
@@ -12,12 +12,16 @@ const PORT = process.env.PORT || 8080;
 // MONGODB CONNECTION
 console.log(process.env.MONGODB_URL);
 
-mongoose.set('strictQuery', false);
+mongoose.set("strictQuery", false);
 
-mongoose.connect("mongodb+srv://laxmanpanjalingam2004:RlffCk5P3KNoLhay@laxmanecommerce.fcesaim.mongodb.net/gadgetgrove?retryWrites=true&w=majority", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+mongoose
+  .connect(
+    "mongodb+srv://laxmanpanjalingam2004:RlffCk5P3KNoLhay@laxmanecommerce.fcesaim.mongodb.net/gadgetgrove?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
   .then(() => {
     console.log("Connected to the database");
   })
@@ -32,11 +36,11 @@ const userSchema = mongoose.Schema({
   email: {
     type: String,
     unique: true,
-    required: true // Make email field required
+    required: true, // Make email field required
   },
   password: String,
   confirmpassword: String,
-  image: String
+  image: String,
 });
 
 // MODEL
@@ -56,17 +60,62 @@ app.post("/signup", async (req, res) => {
     const existingUser = await userModel.findOne({ email: email });
 
     if (existingUser) {
-      
-      res.send({ message: "User with the email already exists" });
+      res.send({ message: "User with the email already exists", alert: false });
     } else {
       const data = new userModel(req.body);
       const savedUser = await data.save();
-      res.send({ message: "Registered successfully", user: savedUser });
+      res.send({
+        message: "Registered successfully",
+        user: savedUser,
+        alert: true,
+      });
     }
   } catch (error) {
     console.error("Error in signup:", error.message);
     res.status(500).send({ message: "Internal server error" });
   }
 });
+
+// login api
+app.post("/login", async (req, res) => {
+  console.log(req.body);
+  const { email, password } = req.body; // Extract both email and password from the request body
+  console.log(email);
+
+  try {
+    const result = await userModel.findOne({ email: email });
+
+    if (result) {
+      // result contains user details as an object
+      console.log(result);
+
+      // Check if the provided password matches the stored password
+      if (password === result.password) {
+        const dataSent = {
+          _id: result._id,
+          fname: result.fname,
+          lname: result.lname,
+          email: result.email,
+          password: result.password,
+          confirmpassword: result,
+          image: result.image,
+        };
+        res.send({
+          message: "Login Successful! Welcome Back",
+          alert: true,
+          data: dataSent,
+        });
+      } else {
+        res.send({ message: "Incorrect password", alert: false });
+      }
+    } else {
+      res.send({ message: "User not found", alert: false });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ message: "Internal Server Error", alert: false });
+  }
+});
+
 
 app.listen(PORT, () => console.log("SERVER IS RUNNING AT PORT: " + PORT));
